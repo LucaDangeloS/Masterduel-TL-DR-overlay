f560c0a4fdd51bf2816d18a0599f4edbcac7215b
LucaDangeloS <l.dangelo@udc.es>
2022-11-04 23:30:21 +0100

    Added missing import

--- Engine/TextProcessing/CardText.cs
+++ Engine/TextProcessing/CardText.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 using TesseractOCR;
