011e7c1a160ab9926e73165242d536cee734f3c5
LucaDangeloS <l.dangelo@udc.es>
2022-11-16 22:54:21 +0100

    Created Overlay form class

--- TLDROverlay.Overlay/Overlay.cs
+++ TLDROverlay.Overlay/Overlay.cs
@@ -1,7 +1,15 @@
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
 namespace TLDROverlay.Overlay
 {
     public partial class Overlay : Form
     {
+        [DllImport("user32.dll")]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
         public Overlay()
         {
             InitializeComponent();
@@ -9,7 +17,29 @@ namespace TLDROverlay.Overlay
 
         private void Overlay_Load(object sender, EventArgs e)
         {
+            BackColor = Color.Wheat;
+            TransparencyKey = Color.Wheat;
+            //FormBorderStyle = FormBorderStyle.None;
+            TopMost = true;
+            StartPosition = FormStartPosition.Manual;
+            ShowInTaskbar = false;
 
+            //int initialStyle = GetWindowLong(Handle, -20);
+            //SetWindowLong(Handle, -20, initialStyle | 0x80000 | 0x20);
         }
+
+        public void SetWindowLocation(Point newLocation)
+        {
+            Location = newLocation;
+            Show();
+        }
+
+        public void SetWindowSize(Size newSize)
+        {
+            Size = newSize;
+            Show();
+        }
+
+        // TODO: Draw Icon at position?
     }
 }
